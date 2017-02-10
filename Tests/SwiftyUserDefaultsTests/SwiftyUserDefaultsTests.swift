90cc1f293339f29d9f326bee175fa1f10bcc7f82
shintaro-morikawa <sntr92@gmail.com>
2017-02-11 00:12:24 +0900

    Add Tests

--- Tests/SwiftyUserDefaultsTests/SwiftyUserDefaultsTests.swift
+++ Tests/SwiftyUserDefaultsTests/SwiftyUserDefaultsTests.swift
@@ -1,36 +1,125 @@
-//
-//  SwiftyUserDefaultsTests.swift
-//  SwiftyUserDefaultsTests
-//
-//  Created by shintaro-morikawa on 2017/02/10.
-//
-//
-
 import XCTest
 @testable import SwiftyUserDefaults
 
+extension UserDefaults.Key {
+    static let someKey = UserDefaults.Key("someKey")
+}
+
 class SwiftyUserDefaultsTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        UserDefaults.standard.removeObject(forKey: UserDefaults.Key.someKey.rawValue)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInit() {
+        let someKey = UserDefaults.Key("someKey")
+        XCTAssertEqual(someKey.rawValue, "someKey")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    func testObjectForKey() {
+        UserDefaults.standard.set(1, forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.object(for: .someKey) as? Int, 1)
+    }
+
+    func testSetAnyForKey() {
+        UserDefaults.standard.set(1, for: .someKey)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: UserDefaults.Key.someKey.rawValue), 1)
+    }
+
+    func testRemoveObjectForKey() {
+        UserDefaults.standard.set(1, forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: UserDefaults.Key.someKey.rawValue), 1)
+        UserDefaults.standard.removeObject(for: .someKey)
+        XCTAssertNil(UserDefaults.standard.object(forKey: UserDefaults.Key.someKey.rawValue))
+    }
+
+    func testStringForKey() {
+        UserDefaults.standard.set("someString", forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.string(for: .someKey), "someString")
+    }
+
+    func testArrayForKey() {
+        UserDefaults.standard.set([1, 2, 3], forKey: UserDefaults.Key.someKey.rawValue)
+        if let array = UserDefaults.standard.array(for: .someKey)?.flatMap({ $0 as? Int }) {
+            XCTAssertEqual(array, [1, 2, 3])
+        } else {
+            XCTFail()
         }
     }
-    
+
+    func testDictionaryForKey() {
+        UserDefaults.standard.set(["key": "value"], forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.dictionary(for: .someKey)?["key"] as? String, "value")
+    }
+
+    func testDataForKey() {
+        let data = "hello".data(using: .utf8)
+        UserDefaults.standard.set(data, forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.data(for: .someKey), data)
+    }
+
+    func testStringArrayForKey() {
+        UserDefaults.standard.set(["a", "b", "c"], forKey: UserDefaults.Key.someKey.rawValue)
+        if let array = UserDefaults.standard.stringArray(for: .someKey) {
+            XCTAssertEqual(array, ["a", "b", "c"])
+        } else {
+            XCTFail()
+        }
+    }
+
+    func testIntegerForKey() {
+        UserDefaults.standard.set(1, forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.integer(for: .someKey), 1)
+    }
+
+    func testFloatForKey() {
+        UserDefaults.standard.set(Float(1.0), forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.float(for: .someKey), Float(1.0))
+    }
+
+    func testDoubleForKey() {
+        UserDefaults.standard.set(Double(1.0), forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.double(for: .someKey), Double(1.0))
+    }
+
+    func testBoolForKey() {
+        UserDefaults.standard.set(true, forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.bool(for: .someKey), true)
+    }
+
+    func testUrlForKey() {
+        let url = URL(string: "https://apple.com")!
+        UserDefaults.standard.set(url, forKey: UserDefaults.Key.someKey.rawValue)
+        XCTAssertEqual(UserDefaults.standard.url(for: .someKey), url)
+    }
+
+    func testSetIntForKey() {
+        UserDefaults.standard.set(1, for: .someKey)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: UserDefaults.Key.someKey.rawValue), 1)
+    }
+
+    func testSetFloatForKey() {
+        UserDefaults.standard.set(Float(1.0), for: .someKey)
+        XCTAssertEqual(UserDefaults.standard.float(forKey: UserDefaults.Key.someKey.rawValue), Float(1.0))
+    }
+
+    func testSetDoubleForKey() {
+        UserDefaults.standard.set(Double(1.0), for: .someKey)
+        XCTAssertEqual(UserDefaults.standard.double(forKey: UserDefaults.Key.someKey.rawValue), Double(1.0))
+    }
+
+    func testSetBoolForKey() {
+        UserDefaults.standard.set(true, for: .someKey)
+        XCTAssertEqual(UserDefaults.standard.bool(forKey: UserDefaults.Key.someKey.rawValue), true)
+    }
+
+    func testSetUrlForKey() {
+        let url = URL(string: "https://apple.com")!
+        UserDefaults.standard.set(url, for: .someKey)
+        XCTAssertEqual(UserDefaults.standard.url(forKey: UserDefaults.Key.someKey.rawValue), url)
+    }
 }
